52cbc518b11a079e5b78258e82f96dde78ecddce
Ariuka140 <ariuka0514@gmail.com>
2018-04-04 18:49:39 +0800

    add

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+ require('./assets/css/bootstrap.min.css')
+ require('./assets/css/font-awesome.min.css')
+ require('./assets/css/flaticon.css')
+ require('./assets/css/font-linearicons.css')
+ require('./style.css')
+ require('./assets/css/travel-setting.css')
+ require('./images/favicon.png')
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
