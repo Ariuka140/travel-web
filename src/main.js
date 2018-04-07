e43871ab0834047a05d8563f09564c7f2899b0aa
Ariuka140 <ariuka0514@gmail.com>
2018-04-07 13:26:36 +0800

    Revert "edit"
    This reverts commit 1dedbdf192c1d1156c123e5bd6ef408eccbe4cff.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import BootstrapVue from 'bootstrap-vue'
-import router from '@/route'
  require('./assets/css/bootstrap.min.css')
  require('./assets/css/font-awesome.min.css')
  require('./assets/css/flaticon.css')
@@ -11,16 +9,11 @@ import router from '@/route'
  require('./style.css')
  require('./assets/css/travel-setting.css')
  require('./images/favicon.png')
- require('flag-icon-css/css/flag-icon.min.css')
- require('bootstrap-vue/dist/bootstrap-vue.css')
- require('./assets/scss/style.scss')
-
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  		router,
   components: { App },
   template: '<App/>'
 })
