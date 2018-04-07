b92c28052002f741d4b926767a22ebe651f81b2b
Uguumur Oldokh <notestyle@yahoo.com>
2018-04-07 18:36:03 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
 	              </div>
 	            </aside>
 	          </div>
-						
+						<div>test</div>
 	          <div class="col-sm-8 topbar-right">
 	            <aside id="text-7" class="widget widget_text">
 	              <div class="textwidget">
