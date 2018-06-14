d7238b425192c23317857562c5af4869513a1666
Uguumur Oldokh <notestyle@yahoo.com>
2018-06-14 16:28:37 +0800

    fix next

--- src/views/AddTour.vue
+++ src/views/AddTour.vue
@@ -95,10 +95,18 @@
     require('./../assets/css/mdb.min.css')
     import db from './../components/firebaseInit'
     import mdb from './../assets/js/mdb.min.js'
-    import axios from './../assets/js/axios.min.js' 
+    import axios from './../assets/js/axios.min.js'
+    import firebase from 'firebase'
     //import * as axios from 'axios';
 
     const BASE_URL = 'http://localhost:8080';
+
+    // firebase.auth().createUserWithEmailAndPassword("email@gmail.com", "pass").catch(function(error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // ...
+    // });
 
     /*function upload(formData) {
         const url = `${BASE_URL}/images/upload  `;
@@ -189,17 +197,69 @@
                         $(this).parents('.entry:first').remove();
                     else
                         $(this).removeClass('cancelBtn').addClass('fileBtn')
-                               .removeClass('orange').addClass('cyan')                         
-                               .html('<i class="fa fa-plus pr-2" aria-hidden="true"></i>');
+                            .removeClass('orange').addClass('cyan')                         
+                            .html('<i class="fa fa-plus pr-2" aria-hidden="true"></i>');
                     e.preventDefault();
                     return false;
                 });
-                },
-                detectFiles(event){
-                    this.selectedFile=event.target.file[0];                 
-                }              
-            }       
-                    
-                    
+            },
+            upload_image_firebase(file){
+                
+                // Get a reference to the storage service, which is used to create references in your storage bucket
+                var storage = firebase.storage();
+
+                // Create a storage reference from our storage service
+                var storageRef = storage.ref();
+
+                // Create the file metadata
+                var metadata = {
+                contentType: 'image/jpeg'
+                };
+
+                // Upload file and metadata to the object 'images/mountains.jpg'
+                var uploadTask = storageRef.child('003/' + file.name).put(file, metadata);
+
+                // Listen for state changes, errors, and completion of the upload.
+                uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                function(snapshot) {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                    case firebase.storage.TaskState.PAUSED: // or 'paused'
+                        console.log('Upload is paused');
+                        break;
+                    case firebase.storage.TaskState.RUNNING: // or 'running'
+                        console.log('Upload is running');
+                        break;
+                    }
+                }, function(error) {
+                    // A full list of error codes is available at
+                    // https://firebase.google.com/docs/storage/web/handle-errors
+                    switch (error.code) {
+                        case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+
+                        case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+
+                        case 'storage/unknown':
+                        // Unknown error occurred, inspect error.serverResponse
+                        break;
+                    }
+                }, function() {
+                    // Upload completed successfully, now we can get the download URL
+                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                        console.log('File available at', downloadURL);
+                    });
+                });
+            },
+            detectFiles(event){
+                var fileToLoad = event.target.files[0];
+                this.upload_image_firebase(fileToLoad);
+            }
+        }   
     }
 </script>
